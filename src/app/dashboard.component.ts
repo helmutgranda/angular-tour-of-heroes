ddb2d796f6486a62bdece44147ea7d799025608b
Helmut Granda <contact@helmutgranda.com>
2017-09-17 00:43:50 -0500

    Reverted to search version.

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -6,7 +6,16 @@ import { HeroService } from './hero.service';
 @Component({
   selector: 'my-dashboard',
   styleUrls: [ './dashboard.component.css' ],
-  templateUrl: './dashboard.component.html'
+  template: `
+  <h3>Top Heroes</h3>
+  <div class="grid grid-pad">
+    <a *ngFor="let hero of heroes"  [routerLink]="['/detail', hero.id]"  class="col-1-4">
+      <div class="module hero">
+        <h4>{{hero.name}}</h4>
+      </div>
+    </a>
+  </div>
+  `
 })
 
 export class DashboardComponent implements OnInit {
